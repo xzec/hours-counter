323c489a4009f6950ae76ef848478456a8566fe2
Juraj Žec <juro.zec.1@gmail.com>
2022-12-01 15:51:59 +0100

    first working version that counts hours

---
+++ README.md
@@ -0,0 +1,35 @@
+## Hours counter
+
+Helps with counting working hours formatted in very opinionated way.
+
+## Run
+```shell
+npm run start
+```
+
+## Example input
+
+```txt
+8h
+7h
+2h 30m<br>2h 30m<br>1h<br><br>1h
+
+
+7h 30m<br>30m
+3h<br>1h<br><br>5h 10m<br>30m
+
+8h
+8h 30m
+```
+
+## Example output
+
+```txt
+┌──────────────┬───────────┐
+│ (idx)        │ Values    │
+├──────────────┼───────────┤
+│ Days         │ 7         │
+│ Hours        │ 56.17     │
+│ Pretty hours │ "56h 10m" │
+└──────────────┴───────────┘
+```
---
+++ index.ts
@@ -0,0 +1,49 @@
+/**
+ * Example input:
+ * 7h 30m
+ * 7h 15m
+ *
+ *
+ * 2h<br><br>2h 30m
+ *
+ * 30m
+ */
+
+const input = await Deno.readTextFile('input.txt')
+
+const dailyEntries = input
+  .split('\n')
+  .filter(Boolean) // remove empty entries (days not working)
+  .map((entry) => entry.trim().replaceAll('<br>', ' ').replace(/  +/g, ' ')) // remove whitespace, <br> elements and reduce multiple spaces to one
+
+const hoursAndMinutes = dailyEntries.join(' ').split(' ') // break down to time entries, e.g. ['1h', '30m', '4h']
+
+const hoursRegex = /^([1-9][0-9]?h)$/
+const minutesRegex = /^([1-9][0-9]?m)$/
+
+const isHours = (entry: string) => entry.match(hoursRegex)
+
+const isMinutes = (entry: string) => entry.match(minutesRegex)
+
+/**
+ * transforms time entries in string format like '1h' or '30m' to hours, always number
+ */
+const timeEntryToHours = (entry: string) => {
+  if (isHours(entry)) return parseInt(entry)
+  else if (isMinutes(entry)) return parseInt(entry) / 60
+  else throw new Error('Wrong input. Entry is not in hours nor minutes.')
+}
+
+const totalHours = hoursAndMinutes.reduce(
+  (hours, entry) => hours + timeEntryToHours(entry),
+  0
+)
+
+const humanReadableTime = (hours: number) =>
+  `${Math.floor(hours)}h ${Math.round((hours % 1) * 60)}m`
+
+console.table({
+  ['Days']: dailyEntries.length,
+  ['Hours']: Math.round(totalHours * 100) / 100,
+  ['Pretty hours']: humanReadableTime(totalHours),
+})
